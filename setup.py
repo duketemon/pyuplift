bf25ede0a5a3dc19df97141cb2811d3f11622432
Artem <duketemon@gmail.com>
2019-03-01 13:42:09 +0500

    added dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+
+with open('requirements.txt', 'r') as f:
+    install_requires = f.read().split('\n')
+
 setup(
     name='pyuplift',
     version='0.0.1',
@@ -8,5 +12,6 @@ setup(
     license='MIT License',
     author='Artem Kuchumov',
     author_email='kuchumov7@gmail.com',
-    description='Uplift modeling implementation'
+    description='Uplift modeling implementation',
+    install_requires=install_requires
 )
