9b6661c250bcf6e39d4b554d0fed7ccdd6f3ab6f
Artem <duketemon@gmail.com>
2019-03-02 13:17:49 +0500

    solved problem with dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
+import os
 from setuptools import setup
 
-
-with open('requirements.txt', 'r') as f:
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirements_path = os.path.join(lib_folder, 'requirements.txt')
+with open(requirements_path, 'r') as f:
     install_requires = f.read().split('\n')
 
 setup(
     name='pyuplift',
-    version='0.0.2',
+    version='0.0.2.1',
     packages=['pyuplift'],
     url='https://github.com/duketemon/pyuplift',
     license='MIT License',
