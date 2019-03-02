39aae893bdda47f3de2848e2b995b6de6a81578d
Artem <duketemon@gmail.com>
2019-03-02 13:56:22 +0500

    changed lib's version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyuplift',
-    version='0.0.2.1',
+    version='0.0.2.2',
     packages=['pyuplift'],
     url='https://github.com/duketemon/pyuplift',
     license='MIT License',
