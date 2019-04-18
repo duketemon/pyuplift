8dc1089ecfdffc3b3fff7875305842c4b5b37b68
Artem <duketemon@gmail.com>
2019-04-18 14:46:53 +0500

    Update load_linear

--- pyuplift/datasets/synthetics.py
+++ pyuplift/datasets/synthetics.py
@@ -4,37 +4,49 @@ import pandas as pd
 
 def load_linear(
         size: int,
-        x1_params=(0, 1),
-        x2_params=(0, 0.5),
-        x3_params=(0, 1),
+        x1_params=(0, 100),
+        x2_params=(0, 10),
+        x3_params=(0, 100),
         t_params=(0, 2),
-        e_params=(0, 0.1),
+        e_params=(0, 100),
+        eps=0.01,
         seed=777
 ):
     """The data generates by formula:
     Y' = X1+X2*T+E
-    Y(Y'<1) = 0
-    Y(Y'>=1) = 1
+    Y = Y', if Y' - int(Y') > eps,
+    Y = 0,  otherwise.
+
+    Statistics for default parameters:
+    ================    ==============
+    Features                         3
+    Treatment                        2
+    Samples total               `size`
+    Y not equals 0             0.50562
+    Y values               0 to 555.93
+    ================    ==============
 
     Parameters
     ----------
     size : int
          The number of observations.
-    x1_params : tuple(mu, sigma), default: (0, 1)
+    x1_params : tuple(mu, sigma), default: (0, 100)
          The feature with gaussian distribution and mean=mu, sd=sigma.
          X1 ~ N(mu, sigma)
-    x2_params : tuple(mu, sigma), default: (0, 0.5)
+    x2_params : tuple(mu, sigma), default: (0, 10)
          The feature with gaussian distribution and mean=mu, sd=sigma.
          X2 ~ N(mu, sigma)
-    x3_params : tuple(mu, sigma), default: (0, 1)
+    x3_params : tuple(mu, sigma), default: (0, 100)
          The feature with gaussian distribution and mean=mu, sd=sigma.
          X3 ~ N(mu, sigma)
     t_params : tuple(min, max), default: (0, 2)
          The treatment with uniform distribution. Min value=min, Max value=max-1
          T ~ R(min, max)
-    e_params : tuple(mu, sigma), default: (0, 0.1)
+    e_params : tuple(mu, sigma), default: (0, 100)
          The error with gaussian distribution and mean=mu, sd=sigma.
          E ~ N(mu, sigma)
+    eps : float, default: 0.01
+         The border value.
     seed : int, default: 777
          The random seed.
     Returns
@@ -48,8 +60,14 @@ def load_linear(
     x3 = np.random.normal(*x3_params, size)
     t = np.random.randint(*t_params, size)
     e = np.random.normal(*e_params, size)
-    y = x1 + x2 * t + e
-    y = np.array([int(v >= 1) for v in y])
+    y_ = x1 + x2 * t + e
+    y = []
+    for value in y_:
+        if value - int(value) > eps:
+            y.append(value)
+        else:
+            y.append(0)
+    y = np.array(y)
     return pd.DataFrame(data={
         'x1': x1,
         'x2': x2,
@@ -57,8 +75,3 @@ def load_linear(
         't': t,
         'y': y
     })
-
-
-if __name__ == '__main__':
-    df = load_linear(10000)
-    print(df.head(10))
